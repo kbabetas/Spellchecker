6390296edb0864153609240ca8f81aeaa58085c9
Ioannis <32537976+IoannisVougias@users.noreply.github.com>
2017-10-22 13:16:42 +0300

    Λειτουργία κλάσης με λεξικό
    Το λεξικό αυτό είναι ένα προσαρμοσμένο λεξικό με 4 λέξεις,το οποίο δουλεύει με συγκεκριμένη έκφραση που δίνει ο χρήστης,στην ουσιαστικά άμα δωσει ο χρήστης την εκφραση Καλημέρα φίλε τι κάνεις με οποιοδηποτε ορθογραφικο θα το βγάλει.Μετα θα ενσωματώσουμε το πραγματικό λεξικό και θα δουλεύει για όλες τις εκφράσεις.

--- Spellchecker.java
+++ Spellchecker.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
-public class Spellchecker {
+public class testSpellchecker {
 
 
 
@@ -12,15 +12,19 @@ public class Spellchecker {
 
 	    	Scanner input = new Scanner(System.in);
 
-		System.out.println("Δώσε κείμενο και θα σου πω άμα είναι ορθογραφικά σωστό\n\n");
+			System.out.println("Δώσε κείμενο και θα σου πω άμα είναι ορθογραφικά σωστό\n\n");
 
 
-        	String keimeno = getGreekInput();           /* Κάλεσμα μεθόδου getGreekInput */
+        	String keimeno = getGreekInput();           			/* Κάλεσμα μεθόδου getGreekInput */
 
-		String[] Pinakas = keimeno.split("[^α-ωΑ-Ω]+");   /* Σπάσιμο του String που έδωσε ο χρήστης σε επιμέρους λέξεις */
+			String[] Pinakas = keimeno.split("[^α-ωΑ-Ωά-ώ]+"); 		/* Σπάσιμο του String που έδωσε ο χρήστης σε επιμέρους λέξεις */
+			String[] correct = createCorrectWordsArray(Pinakas.length);   /*ο πινακας που έχει μέσα τις λέξεις του μινι λεξικού */
+			String[] wrong = binarySearch(Pinakas ,correct); 			  /*ο πινακας που έχει μέσα τις λάθος λεξεις που έδωσε ο χρήστης,σε σχέση με το μινι λεξικό,χρησιμοποιόντας την binarySearch */
+			printArray(Pinakas);										  /* εκτύπωση των λέξεων που έδωσε ο χρήστης */
 
-		  
-		printArray(Pinakas);
+
+			System.out.println("Οι λάθος λέξεις είναι :\n\n");
+			printArray(wrong);   										/* εκτύπωση του πινακα με τις λαθος λεξεις */
 
 
 
@@ -28,32 +32,87 @@ public class Spellchecker {
 
 
 	}
-	
-	
+
+
 	private static String getGreekInput() {    /* Μέθοδος που σου επιτρέπει να διαβάζεις Strings στα ελληνικά */
+
 	        System.out.flush();          		/* Καθαρίζει τη μνήμη  */
 
-	        try {                       
+	        try {
 	            BufferedReader in = new BufferedReader(new InputStreamReader(System.in, "cp737"));    /* Χειρισμός εξαίρεσης  */
 	            return in.readLine();
 	        } catch (Exception e) {
 	            return "Error: " + e.getMessage();
 	        }
+
 	}  /* Τέλος μεθόδου getGreekInput */
 
-	
-	
-	private static void printArray(String[] pinakas) {    /* Μέθοδος που εκτυπώνει τον πίνακα στον οποίο έχουν καταχωρηθεί οι λέξεις του String */
+
+
+	private static void printArray(String[] pinakas) {    /* Μέθοδος που εκτυπώνει  πίνακα  */
 
 		  for  (int i=0 ; i< pinakas.length;i ++) {
-			   	System.out.println("\n");            /* Κενά */
+			    if(pinakas[i] != null) {
+			   	System.out.println("\n");			 /* Κενά */
 			   	System.out.println(pinakas[i]);      /* Εκτύπωση του πίνακα */
-            			System.out.println("\n");            /* Κενά */
+            	System.out.println("\n");            /* Κενά */
+				}
 
    		 }
 
 	} /* Τέλος μεθόδου printArray */
-    
+
+	private static String[] createCorrectWordsArray(int N) {  /*Μέθοδος που δημιουργεί ένα προκαθορισμένο λεξικό,στη συνέχεια θα βάλουμε το κανονικό */
+				String[] array1 = new String[N];
+
+				array1[0] = "Καλημέρα" ;
+				array1[1] = "φίλε" ;
+				array1[2] = "τι" ;
+				array1[3] = "κάνεις" ;
+
+				return array1 ;
+
+
+
+
+	} /* Τέλος μεθόδου createCorrectWordsArray */
+
+
+	private static String[] binarySearch(String [] a ,String [] b) { /* Δυαδική αναζήτηση σε 2 πίνακες,γεμίζοντας έναν τρίτο με τα στοιχεία που δεν είναι κοινά και στους 2 */
+
+			String[] wrongArray = new String [a.length];
+			int wrongIndex= 0 ;
+			for(int i = 0; i < a.length; i++) {
+			  	int finder = 0 ;
+
+		    	for(int j = 0; j < b.length; j++) {
+
+		       		if(a[i].equals(b[j])) {
+
+		            	finder=1;
+		        	}
+		   	    }
+
+		   	    if( finder == 0) {
+					wrongArray[wrongIndex] = a[i];
+					wrongIndex++;
+				}
+
+			}
+
+			return wrongArray ;   /* Επιστρέφει τον πίνακα με τις λάθος λέξεις */
+
+
+
+
+
+
+
+
+		} /* Τέλος μεθόδου binarySearch */
+
+
+
 
 
 
