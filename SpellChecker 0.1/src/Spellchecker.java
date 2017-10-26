28abeef14cf541dd68b9fd114f5708fe66fc1745
Ioannis <32537976+IoannisVougias@users.noreply.github.com>
2017-10-26 22:14:29 +0300

    Δημιουργία αντικειμένου Builddictionary
    Διαγραφη των περιττων μεθόδων,δημιουργία αντικειμένου Builddictionary,καλεσμα της μεθόδου buildDictionary() και περασμα του dictionary ως ορισμα στην αναζητηση treeSearch που δεν εχει υλοποιηθει ακομα

--- SpellChecker 0.1/src/Spellchecker.java
+++ SpellChecker 0.1/src/Spellchecker.java
@@ -1,6 +1,8 @@
 import java.util.Scanner;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.Map;
+import java.util.TreeMap;
 
 public class Spellchecker {
 
@@ -18,13 +20,15 @@ public class Spellchecker {
         	String keimeno = getGreekInput();           			/* Κάλεσμα μεθόδου getGreekInput */
 
 			String[] Pinakas = keimeno.split("[^α-ωΑ-Ωά-ώ]+"); 		/* Σπάσιμο του String που έδωσε ο χρήστης σε επιμέρους λέξεις */
-			String[] correct = createCorrectWordsArray(Pinakas.length);   /*ο πινακας που έχει μέσα τις λέξεις του μινι λεξικού */
-			String[] wrong = binarySearch(Pinakas ,correct); 			  /*ο πινακας που έχει μέσα τις λάθος λεξεις που έδωσε ο χρήστης,σε σχέση με το μινι λεξικό,χρησιμοποιόντας την binarySearch */
+			BuildDictionary builder = new BuildDictionary();
+			Map<Integer, String> dictionary = builder.buildDictionary();
+
+
 			printArray(Pinakas);										  /* εκτύπωση των λέξεων που έδωσε ο χρήστης */
 
 
 			System.out.println("Οι λάθος λέξεις είναι :\n\n");
-			printArray(wrong);   										/* εκτύπωση του πινακα με τις λαθος λεξεις */
+
 
 
 
@@ -62,41 +66,10 @@ public class Spellchecker {
 
 	} /* Τέλος μεθόδου printArray */
 
-	private static String[] createCorrectWordsArray(int N) {  /*Μέθοδος που δημιουργεί ένα προκαθορισμένο λεξικό,στη συνέχεια θα βάλουμε το κανονικό */
-				String[] array1 = new String[N];
 
-				array1[0] = "Καλημέρα" ;
-				array1[1] = "φίλε" ;
-				array1[2] = "τι" ;
-				array1[3] = "κάνεις" ;
-
-				return array1 ;
-
-
-
-
-	} /* Τέλος μεθόδου createCorrectWordsArray */
-
-
-	private static String[] binarySearch(String [] a ,String [] b) { /* Δυαδική αναζήτηση σε 2 πίνακες,γεμίζοντας έναν τρίτο με τα στοιχεία που δεν είναι κοινά και στους 2 */
-
-			String[] wrongArray = new String [a.length];
-			int wrongIndex= 0 ;
+	private static String[] treeSearch(String [] a , Map<Integer, String> dic) { /*  Ανα \ήτηση σε 2 πίνακες,γεμίζοντας έναν τρίτο με τα στοιχεία που δεν είναι κοινά και στους 2 */
+			String[] wrongArray = new String[a.length];
 			for(int i = 0; i < a.length; i++) {
-			  	int finder = 0 ;
-
-		    	for(int j = 0; j < b.length; j++) {
-
-		       		if(a[i].equals(b[j])) {
-
-		            	finder=1;
-		        	}
-		   	    }
-
-		   	    if( finder == 0) {
-					wrongArray[wrongIndex] = a[i];
-					wrongIndex++;
-				}
 
 			}
 
@@ -109,7 +82,7 @@ public class Spellchecker {
 
 
 
-		} /* Τέλος μεθόδου binarySearch */
+		} /* Τέλος μεθόδου treeSearch */
 
 
 
