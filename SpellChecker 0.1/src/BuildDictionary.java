e82c0487df855a36081c506a6f0631b026abbf58
Konstantinos Babetas <kbabetas@gmail.com>
2017-10-26 19:02:03 +0300

    Μετατροπή buildDictionary σε μέθοδο

--- SpellChecker 0.1/src/BuildDictionary.java
+++ SpellChecker 0.1/src/BuildDictionary.java
@@ -6,24 +6,23 @@ import java.io.IOException;
 
 /* Μετατρέπει το λεξικό μας (Dictionary.txt) σε ένα treemap με όνομα dictionary */
 
-public class BuildDictionary {
 	
-	public static void main(String[] args) {
+public static void buildDictionary() {
 		
-		Map<Integer, String> dictionary = new TreeMap<Integer, String>();
-		try {
-			BufferedReader reader = new BufferedReader(new FileReader("Dictionary.txt")); /* Dictionary.txt should be on the same folder  */ 
-            String word;
-            int key = 0;
-            while ((word = reader.readLine()) != null) {
-            	dictionary.put(key, word);
-            	key++;
-            }
+	Map<Integer, String> dictionary = new TreeMap<Integer, String>();
+	try {
+		BufferedReader reader = new BufferedReader(new FileReader("Dictionary.txt")); /* Dictionary.txt should be on the same folder  */ 
+            	String word;
+            	int key = 0;
+            	while ((word = reader.readLine()) != null) {
+            		dictionary.put(key, word);
+            		key++;
+            	}
         } catch (IOException ioe) {
            ioe.printStackTrace();
         }
 		
-	}
-
 }
+
+
 	
