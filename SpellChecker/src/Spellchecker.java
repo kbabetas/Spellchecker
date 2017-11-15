8ca0428bf3c7a53ee5e29758dfd4e885050828fc
Ioannis <32537976+IoannisVougias@users.noreply.github.com>
2017-11-15 15:46:08 +0200

    getMenu method added

--- SpellChecker/src/Spellchecker.java
+++ SpellChecker/src/Spellchecker.java
@@ -103,6 +103,35 @@ public class Spellchecker {
 			
 				
        } /* Τέλος μεθόδου treeSearch */
+	
+	private static int getMenu() {
+
+
+
+		System.out.println("Press 1 for Greek:\nPress 2 for English:\n");
+
+		int choice = input.nextInt();
+
+		while( (choice != 1) && (choice != 2) ) {
+		  		System.out.print("You should choose either 1 or 2\n");
+		        System.out.print("Press 1 for Greek:\nPress 2 for English:\n");
+		        choice = input.nextInt();
+      		}
+
+		if(choice == 1) {
+		System.out.println("Πληκτρολόγησε ένα κείμενο στα ελληνικά και θα ελέγξουμε άμα είναι ορθογραφικά σωστό:\n\n");
+
+
+		} else {
+		System.out.println("Type a text in English and we will check if the spelling is correct:\n\n");
+
+
+		}
+
+		return choice;
+
+	}  /* Τέλος getMenu */
+
 
 
 
