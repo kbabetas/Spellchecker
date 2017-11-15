4b7ea2d66e7bac97f332306678936fe9b0c63a57
MarinaKakoulaki <32638862+MarinaKakoulaki@users.noreply.github.com>
2017-11-15 17:09:41 +0200

    Add files via upload
    Έλεγχος splitArray 2

---
+++ Tests/SplitTest2.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+
+public class SplitTest2 {
+
+	public static void main (String[] args) {
+
+		String text = "δοκιμή με    κενό";
+
+		String[] splittedArray = text.split("[^α-ωΑ-Ωά-ώ]+");
+
+		String[] array = splittedArray;
+
+		if (array[2].equals("κενό")) {
+			System.out.println("well done team!");
+			System.out.println("To programma douleuei osa kena kai na exei anamesa stis le3eis");
+		} else {
+			System.out.println("try again");
+			System.out.println(array[0]);
+		}
+
+		for  (int i=0 ; i< array.length;i ++) {
+			if(array[i] != null) {
+				System.out.println();
+				System.out.println(array[i]);
+		        System.out.println("\n");
+			}
+		}
+	}
+}
