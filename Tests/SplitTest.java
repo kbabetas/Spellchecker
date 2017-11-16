8b55b065b47183a5b73e42aa85beaff2002386c1
MarinaKakoulaki <32638862+MarinaKakoulaki@users.noreply.github.com>
2017-11-16 18:06:15 +0200

    Update SplitTest.java

--- Tests/SplitTest.java
+++ Tests/SplitTest.java
@@ -4,24 +4,46 @@ public class SplitTest {
 
 	public static void main (String[] args) {
 
-		String text = "Γεια σας παιδιά!";
+		String text1 = "Γεια σας παιδιά!";
+		String text2 = "δοκιμή με    κενό";
 
-		String[] splittedArray = text.split("[^á-ùÁ-ÙÜ-þ]+");
+		String[] splittedArray1 = text1.split("[^α-ωΑ-Ωά-ώ]+");
+		String[] splittedArray2 = text2.split("[^α-ωΑ-Ωά-ώ]+");
 
-		String[] array = splittedArray;
+		String[] array1 = splittedArray1;
+		String[] array2 = splittedArray2;
 
-		if (array[0].equals("Γεια")) {
+		if (array1[0].equals("Γεια")) {
+			System.out.println("try 1:");
 			System.out.println("well done team!");
+			System.out.println("H methodos splitArray douleuei kanonika");
 		} else {
 			System.out.println("try again");
-			System.out.println(array[0]);
+			System.out.println(array1[0]);
 		}
 
-		for  (int i=0 ; i< array.length;i ++) {
-			if(array[i] != null) {
+		for  (int i=0 ; i< array1.length;i ++) {
+			if(array1[i] != null) {
 				System.out.println();
-				System.out.println(array[i]);
+				System.out.println(array1[i]);
 		        System.out.println("\n");
+			}
+		}
+
+		if (array2[2].equals("κενό")) {
+			System.out.println("try 2:");
+			System.out.println("well done team!");
+			System.out.println("To programma douleuei osa kena kai na exei anamesa stis le3eis");
+		} else {
+			System.out.println("try again");
+			System.out.println(array2[2]);
+		}
+
+		for  (int i=0 ; i< array2.length;i ++) {
+			if(array2[i] != null) {
+				System.out.println();
+				System.out.println(array2[i]);
+			    System.out.println("\n");
 			}
 		}
 	}
