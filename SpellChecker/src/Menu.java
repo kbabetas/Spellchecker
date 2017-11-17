d753d90b62a82de0e4a913716ed6312257e11a0a
Ioannis <32537976+IoannisVougias@users.noreply.github.com>
2017-11-17 14:21:08 +0200

    Update Menu.java

--- SpellChecker/src/Menu.java
+++ SpellChecker/src/Menu.java
@@ -5,13 +5,59 @@ public class Menu {
 	public static void main(String args[]) {
 
 		Spellcheck Spell = new Spellcheck();
-		int choice = Spell.getMenu();
-		String text = Spell.getGreekInput();
+		int choice = getMenu();
+		String text = getGreekInput();
 		System.out.printf("\n");
 		Spell.spellcheck(choice,text);
 		
 
 	}
+	
+	
+	public static int getMenu() {
+
+
+
+			System.out.println("Press 1 for Greek:\nPress 2 for English:\n");
+
+			int choice = input.nextInt();
+
+			while( (choice != 1) && (choice != 2) ) {
+			  		System.out.print("You should choose either 1 or 2\n");
+			        System.out.print("Press 1 for Greek:\nPress 2 for English:\n");
+			        choice = input.nextInt();
+	      		}
+
+			if(choice == 1) {
+			System.out.println("Πληκτρολόγησε ένα κείμενο στα ελληνικά και θα ελέγξουμε άμα είναι ορθογραφικά σωστό:\n\n");
+
+
+
+			} else {
+			System.out.println("Type a text in English and we will check if the spelling is correct:\n\n");
+
+
+			}
+
+			return choice;
+
+	}  /* Τέλος getMenu */
+	
+	public static String getGreekInput() {    /* Μέθοδος που σου επιτρέπει να διαβάζεις Strings στα ελληνικά */
+
+		        System.out.flush();          		/* Καθαρίζει τη μνήμη  */
+
+		        try {
+		            BufferedReader in = new BufferedReader(new InputStreamReader(System.in, "cp737"));    /* Χειρισμός εξαίρεσης  */
+		            return in.readLine();
+		        } catch (Exception e) {
+		            return "Error: " + e.getMessage();
+		        }
+
+	}  /* Τέλος μεθόδου getGreekInput */
+
+	
+	
 
 
 }
