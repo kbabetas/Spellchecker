2f7a4f2978242946887ce20083ee5cecba43026b
Konstantinos Babetas <kbabetas@gmail.com>
2017-11-17 14:24:55 +0200

    Remove GetMenu & GetInput
    They have been moved to Menu class

--- SpellChecker/src/Spellcheck.java
+++ SpellChecker/src/Spellcheck.java
@@ -1,6 +1,3 @@
-import java.util.Scanner;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.util.Map;
 import java.util.TreeMap;
 
@@ -34,25 +31,6 @@ public class Spellcheck {
 		} 
 
 	}
-
-	/**
-	  *	Μέθοδος που σου επιτρέπει να διαβάζεις Strings.
-	  *	@return the text that the user inserted
-	  *	@throws Exception if the input is wrong.
-	  */
-	private static String getInput() {    
-
-	        System.out.flush();          		/* Καθαρίζει τη μνήμη  */
-
-	        try {
-	            BufferedReader in = new BufferedReader(new InputStreamReader(System.in, "cp737"));    /* Χειρισμός εξαίρεσης  */
-	            return in.readLine();
-	        } catch (Exception e) {
-	            return "Error: " + e.getMessage();
-	        }
-
-	}  /* Τέλος μεθόδου getInput */
-
 
 	/**
 	  *	Μέθοδος που εκτυπώνει  πίνακα
@@ -101,39 +79,5 @@ public class Spellcheck {
 				
        } /* Τέλος μεθόδου treeSearch */
 	
-	private static int getMenu() {
-
-
-
-		System.out.println("Press 1 for Greek:\nPress 2 for English:\n");
-
-		int choice = input.nextInt();
-
-		while( (choice != 1) && (choice != 2) ) {
-		  		System.out.print("You should choose either 1 or 2\n");
-		        System.out.print("Press 1 for Greek:\nPress 2 for English:\n");
-		        choice = input.nextInt();
-      		}
-
-		if(choice == 1) {
-		System.out.println("Πληκτρολόγησε ένα κείμενο στα ελληνικά και θα ελέγξουμε άμα είναι ορθογραφικά σωστό:\n\n");
-
-
-		} else {
-		System.out.println("Type a text in English and we will check if the spelling is correct:\n\n");
-
-
-		}
-
-		return choice;
-
-	}  /* Τέλος getMenu */
-
-
-
-
-
-
-
 } /* Τέλος κλάσης */
 
