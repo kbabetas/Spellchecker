70bffc29c427b2e30ef10c94dbbca7cdd86592ff
Ioannis <32537976+IoannisVougias@users.noreply.github.com>
2017-11-17 13:47:19 +0200

    Update Menu.java

--- SpellChecker/src/Menu.java
+++ SpellChecker/src/Menu.java
@@ -8,28 +8,10 @@ public class Menu {
 		int choice = Spell.getMenu();
 		String text = Spell.getGreekInput();
 		System.out.printf("\n");
-
-		String[] words = text.split("[^a-zA-Zα-ωΑ-Ωά-ώΐ]+"); 	
+		Spell.spellchecker(choice,text);
 		
-		System.out.printf("\n\n");
-
-		Map<Integer, String> dictionary = BuildDictionary.buildDictionary(choice);
-
-
-		String[] wrongwords = Spell.treeSearch(words,dictionary);
-
-		System.out.printf("\n");
-		if (wrongwords[0] == null && wrongwords[1] == null) {
-			System.out.println("Το κείμενο είναι ορθογραφικά σωστό.\n\n");
-		} else {
-			System.out.println("Οι λάθος λέξεις είναι :\n\n");
-			Spell.printArray(wrongwords);
-		}
-
 
 	}
-
-
 
 
 }
