1ea1b4f0a3e0eb240f3e79de3e7688248c5d9045
Ioannis <32537976+IoannisVougias@users.noreply.github.com>
2017-11-19 23:20:41 +0200

    Διαγραφή αντικειμένου

--- SpellChecker/src/Spellcheck.java
+++ SpellChecker/src/Spellcheck.java
@@ -9,7 +9,7 @@ import java.util.TreeMap;
   */ 
 public class Spellcheck {
 	
-	private static Scanner input = new Scanner(System.in);
+	
 
 
 	public static void spellcheck(int choice, String text) {
