b45c92460b51c7a621f5ff43b54fbc908a15696f
Christianna Georgouli <32618561+javalicious@users.noreply.github.com>
2017-11-24 14:42:50 +0200

    Μορφοποίηση Menu

--- SpellChecker/src/main/java/gr/aueb/dmst/dirtybits/Menu.java
+++ SpellChecker/src/main/java/gr/aueb/dmst/dirtybits/Menu.java
@@ -12,26 +12,21 @@ public class Menu {
 	
 	private static int selection;  //shows how the user selects to give the text
 	private static int language;  //shows the user's choice either Greek or English
-	
-
 	private static Scanner input = new Scanner(System.in);
-	
 	
 	public static void main(String args[]) {
 		setEncoding();
 		Spellcheck Spell = new Spellcheck();
 		language = getMenu();
 		String text = null;
-		 if(selection == 1) {
+		 if (selection == 1) {
 			text = getInput();
 		 } else {
 			text = getFileReader();
 		 }
 		
-		System.out.printf("\n");
+		System.out.println();
 		Spell.spellcheck(language,text);
-		
-
 	}
 	
 	/**
@@ -43,76 +38,61 @@ public class Menu {
 		System.out.println("Press 1 for Greek/Πιέστε 1 για ελληνικά:\nPress 2 for English/Πιέστε 2 για αγγλικά:\n");
 
 		int language = input.nextInt();
-		System.out.print("\n");
+		System.out.println();
 
-		while( (language != 1) && (language != 2) ) {
+		while ((language != 1) && (language != 2)) {
 			  System.out.print("Please press 1 or 2\n");
 			  System.out.print("Press 1 for Greek:\nPress 2 for English:\n");
 			  language = input.nextInt();
 	      	}
 
-		if(language == 1) {
-			System.out.println("Πληκτρολόγησε 1 άμα θέλεις να γράψεις  ένα δικό σου  κείμενο :");
+		if (language == 1) {
+			System.out.println("Πληκτρολόγησε 1 άμα θέλεις να γράψεις ένα δικό σου  κείμενο:");
 			System.out.println("Πληκτρολόγησε 2 άμα θέλεις να ελέγξεις ορθογραφικά ένα αρχείο της επιλογής σου:\n");
 
 			selection = input.nextInt();
-			System.out.print("\n");
-			while( (selection != 1) && (selection != 2) ) {
+			System.out.println();
+			while ((selection != 1) && (selection != 2)) {
 				System.out.print("Παρακαλώ πληκτρολογήστε 1 ή 2\n");
-
 				selection = input.nextInt();
-				System.out.print("\n");
+				System.out.println();
 	      		}
-
-
-			if(selection == 1) {
+			if (selection == 1) {
 				System.out.println("Πληκτρολόγησε ένα κείμενο στα ελληνικά και θα ελέγξουμε άμα είναι ορθογραφικά σωστό:\n\n");
 			} else {
 				System.out.println("Πληκτρολόγησε το path που βρίσκεται το αρχείο και θα ελέγξουμε άμα είναι ορθογραφικά σωστό:\n");
 			}
-
-
-
 		} else {
-			System.out.println("Press 1  if you want to test the spelling of a text that you will type:");
-			System.out.println("Press 2  if you want to test the spelling of a file that you will choose:\n");
+			System.out.println("Press 1 if you want to test the spelling of a text that you will type:");
+			System.out.println("Press 2 if you want to test the spelling of a file that you will choose:\n");
 
 			selection = input.nextInt();
-			System.out.print("\n");
-			while( (selection != 1) && (selection != 2) ) {
+			System.out.println();
+			while ((selection != 1) && (selection != 2)) {
 				System.out.print("Please press 1 or 2\n");
-
 				selection = input.nextInt();
-				System.out.print("\n");
+				System.out.println();
 	      		}
-			if(selection == 1) {
+			if (selection == 1) {
 				System.out.println("Type a text in English and we will check if the spelling is correct:\n\n");
 			} else {
 				System.out.println("Please insert the path of the file and we will check if the spelling is correct:\n");
-			 }
-
-
-
+			}
 		}
-
 		return language;
-		
-
-
-
 	}  /* end of method getMenu */
 	
-	public static String getInput() {    /*  with this method you can read Strings in Greek */
+	public static String getInput() {    
+	/*  with this method you can read Strings in Greek */
 
-		        System.out.flush();          		/* clear the memory */
-
-		        try {
-		            BufferedReader in = new BufferedReader(new InputStreamReader(System.in, "cp737"));    /*handle exception */
-		            return in.readLine();
-		        } catch (Exception e) {
-		            return "Error: " + e.getMessage();
-		        }
-
+		System.out.flush();          		
+		/* clear the memory */
+		try {
+			BufferedReader in = new BufferedReader(new InputStreamReader(System.in, "cp737"));    /*handle exception */
+			return in.readLine();
+		} catch (Exception e) {
+			return "Error: " + e.getMessage();
+		}
 	}  /* end of method getGreekInput */
 
 	/**
@@ -123,51 +103,36 @@ public class Menu {
 	 */
 	public static String getFileReader()  {
 
-			input.nextLine();
+		input.nextLine();
+		String path = input.nextLine();
+		String text = null;
+		try {
+			String[] printLines =  getFile(path);
 
-			String path = input.nextLine();
-		    	String text = null;
-
-			try {
-
-				String[] printLines =  getFile(path);
-
-
-
-
-				for (int i = 0;i < printLines.length; i++) {
-					text += " " + printLines[i];
-				}
-			
-				System.out.println();
-			
-				if (language == 2) {
-					System.out.println("The file was read successfully");
-				} else {
-					System.out.println("Το αρχείο διαβάστηκε επιτυχώς");
-
-				}
-
-
-			
-			
-			} catch (IOException e) {
-				System.out.println("Sorry, we cannot find your text file. Please try again." );
-
+			for (int i = 0; i < printLines.length; i++) {
+				text += " " + printLines[i];
 			}
 			
-			String newtext= text.substring(4);
-			
-			if (language == 2 ) {
-			System.out.println("The context of your file is:");
+			System.out.println();
+			if (language == 2) {
+				System.out.println("The file was read successfully");
 			} else {
-			System.out.println("Το περιεχόμενο του αρχείου είναι:\n");
-
+				System.out.println("Το αρχείο διαβάστηκε επιτυχώς");
 			}
+		} catch (IOException e) {
+			System.out.println("Sorry, we cannot find your text file. Please try again." );
+		}
+			
+		String newtext= text.substring(4);
+			
+		if (language == 2 ) {
+			System.out.println("The context of your file is:\n");
+		} else {
+			System.out.println("Το περιεχόμενο του αρχείου είναι:\n");
+		}
 
-			System.out.println(newtext);
-
-			return newtext;
+		System.out.println(newtext);
+		return newtext;
 	} // end of method getFileReader
 	
 	/**
@@ -185,14 +150,11 @@ public class Menu {
 		int numberOfLines = calculateLines(path);
 	   	String[] textOfData = new String[numberOfLines]; //each position in textData array can hold one line of text.
 	
-	   	for (int i=0; i< numberOfLines; i++) {
+	   	for (int i=0; i < numberOfLines; i++) {
 	    		textOfData[i] = textReader.readLine();
-	    	
 	    	}
-	    
-	    textReader.close();
-	    return textOfData;
-	
+		textReader.close();
+		return textOfData;
 	} //end of method getFile
 	
 	/**
@@ -202,19 +164,13 @@ public class Menu {
 	 */
 	public static int calculateLines(String path) throws IOException{
 		
-		
 		FileReader fr = new FileReader(path);
 		BufferedReader textReader = new BufferedReader(fr);
-		
-		
 		int numberOfLines = 0;
-		
-		while ( textReader.readLine()  != null ) {
+		while (textReader.readLine() != null) {
 			numberOfLines++ ;
 		}
-		
 		textReader.close();
-		
 		return numberOfLines;
 	} //end of method calculateLines
 	
@@ -233,7 +189,4 @@ public class Menu {
 			ioe.printStackTrace();
 		}
 	}
-	
-
-
 }
