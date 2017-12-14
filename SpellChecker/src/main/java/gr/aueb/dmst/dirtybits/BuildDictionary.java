47c38b947bc77a2f39f47ec04af27aaade5bf003
Konstantinos Babetas <kbabetas@gmail.com>
2017-12-14 16:16:25 +0200

    BuildDictionary constructor & getDictionary addition
    Removed buildDictionary method
    Added constructor & getDictionary and made the dictionary private

--- SpellChecker/src/main/java/gr/aueb/dmst/dirtybits/BuildDictionary.java
+++ SpellChecker/src/main/java/gr/aueb/dmst/dirtybits/BuildDictionary.java
@@ -15,35 +15,36 @@ import java.io.File;
  */
 public class BuildDictionary {
 
-/**
- * This method encodes the dictionary and after puts every word in a treemap
- * @param choice shows which of the two dictionaries the user will need.
- * @return the appropriate dictionary in a treemap
- */
-	public static Map<Integer, String>  buildDictionary(int language) {
-
-		Map<Integer, String> dictionary = new TreeMap<Integer, String>();
+	private static Map<Integer, String> dictionary = new TreeMap<Integer, String>();
+	private static final String[] DICTIONARIES = {"Greek Dictionary.txt", "English Dictionary.txt"};
+	/**
+	 * This method encodes the dictionary and after puts every word in a treemap
+	 * @param language shows which of the two dictionaries the user will need
+	 * */
+	public BuildDictionary(int language) {
 		File upOne = new File(System.getProperty("user.dir")).getParentFile();
 
 		try {
 			BufferedReader reader;
-			if (language == 1) {
-				reader = new BufferedReader(new InputStreamReader(new FileInputStream(upOne + "/resources/Greek Dictionary.txt"),"UTF-8"));
-			} else if (language == 2) {
-				reader = new BufferedReader(new InputStreamReader(new FileInputStream(upOne + "/resources/English Dictionary.txt"),"UTF-8"));
-			} else {
-				return null;
-			}
+			reader = new BufferedReader(new InputStreamReader(new FileInputStream(upOne + "/resources/" + DICTIONARIES[language - 1]),"UTF-8")); // for non maven use
+			//reader = new BufferedReader(new InputStreamReader(new FileInputStream(upOne + "/src/main/resources/" + DICTIONARIES[language - 1]),"UTF-8")); for maven
 
-           		String word;
-            		int key = 0;
-            		while ((word = reader.readLine()) != null) {
-            			dictionary.put(key, word);
-            			key++;
-           		}
+           	String word;
+            int key = 0;
+            while ((word = reader.readLine()) != null) {
+            	dictionary.put(key, word);
+            	key++;
+           	}
+            reader.close();
 		} catch (IOException ioe) {
 			ioe.printStackTrace();
 		}
-		return dictionary;
+	}
+	/*
+	 * This method returns the dictionary
+	 * @return the appropriate dictionary in a treemap
+	 */
+	public Map<Integer, String> getDictionary() {
+		return BuildDictionary.dictionary;
 	}
 }
