8f9b1fa1fcab76985a768f3d4832083e8ff2e114
Konstantinos Babetas <kbabetas@gmail.com>
2017-12-30 19:07:50 +0200

    Addition of GreekEncodingCMD Class
    Sets cmd encoding to cp737

---
+++ SpellChecker/src/main/java/gr/aueb/dmst/dirtybits/GreekEncodingCMD.java
@@ -0,0 +1,23 @@
+package gr.aueb.dmst.dirtybits;
+
+import java.io.IOException;
+
+public class GreekEncodingCMD {
+	
+	public static void main(String[] args) {
+		setEncoding();	
+	}
+
+	public static void setEncoding() {
+		try {
+			ProcessBuilder builder = new ProcessBuilder("cmd.exe", "/c", "chcp", "737").inheritIO();
+			Process p = builder.start();
+			p.waitFor();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+			
+		} catch (IOException ioe) {
+			ioe.printStackTrace();
+		}
+	}
+}
