06c5abd538e3b78bfcd907f77d5b579f4799fdbe
Konstantinos Babetas <kbabetas@gmail.com>
2018-01-02 13:35:17 +0200

    GreekEncodingCMD code quality fix

--- SpellChecker/src/main/java/gr/aueb/dmst/dirtybits/GreekEncodingCMD.java
+++ SpellChecker/src/main/java/gr/aueb/dmst/dirtybits/GreekEncodingCMD.java
@@ -3,19 +3,20 @@ package gr.aueb.dmst.dirtybits;
 import java.io.IOException;
 
 public class GreekEncodingCMD {
-	
+
 	public static void main(String[] args) {
-		setEncoding();	
+		setEncoding();
 	}
 
 	public static void setEncoding() {
 		try {
-			ProcessBuilder builder = new ProcessBuilder("cmd.exe", "/c", "chcp", "737").inheritIO();
+			ProcessBuilder builder = new ProcessBuilder("cmd.exe", "/c",
+					"chcp", "737").inheritIO();
 			Process p = builder.start();
 			p.waitFor();
 		} catch (InterruptedException e) {
 			e.printStackTrace();
-			
+
 		} catch (IOException ioe) {
 			ioe.printStackTrace();
 		}
