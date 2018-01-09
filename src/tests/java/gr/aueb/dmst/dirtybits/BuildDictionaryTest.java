95105f6b32469b359f88914e15cbcc976c3a2926
Konstantinos Babetas <kbabetas@gmail.com>
2018-01-09 20:37:36 +0200

    BuildDictionaryTest testLastWordAddition

--- src/tests/java/gr/aueb/dmst/dirtybits/BuildDictionaryTest.java
+++ src/tests/java/gr/aueb/dmst/dirtybits/BuildDictionaryTest.java
@@ -1,26 +1,30 @@
 package gr.aueb.dmst.dirtybits;
 
 import static org.junit.Assert.*;
-import java.util.Map;
-import org.junit.Test;
+
 import java.io.IOException;
+import java.util.Map;
+
+import org.junit.Test;
 
 public class BuildDictionaryTest {
-
 	int language = 2;
 	BuildDictionary object = new BuildDictionary(language);
 
-
 	Map<Integer, String> dictionary = object.getDictionary();
-	int size = dictionary.size() - 1;
-
 	String firstWord = "a";
+	int size = dictionary.size() - 1;
+	String lastWord = "zwitterionic";
 
 	@Test
 	public void testFirstWord() throws IOException {
-
 		assertEquals(firstWord, dictionary.get(0));
+	}
 
+	@Test
+	public void testLastWord() throws IOException {
+		assertEquals(lastWord, dictionary.get(size));
 	}
 
 }
+
